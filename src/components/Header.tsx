c4e4f02f46f707b390ce7396bbe3a7c33cb5e371
MarianaSavoldi <marianasavodi123@gmail.com>
2024-02-27 15:56:08 -0300

    feat: create Header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+import headerLogo from '../assets/header-logo.png'
+import styles from './Header.module.css'
+
+export function Header() {
+  return (
+    <header className={styles.header}>
+      <img src={headerLogo} alt="" />
+    </header>
+  )
+}
